9f1565452e77b98ea35bd4a141b914257af269f5
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2022-03-16 23:11:31 +0100

    wip

--- maintenance/pip/build_wheels.sh
+++ maintenance/pip/build_wheels.sh
@@ -7,8 +7,8 @@ rm -rf wheels_temp wheels
 mkdir wheels_temp
 mv pyproject.toml pyproject.toml.orig
 for x in `ls /opt/python` ; do
-    cp maintenance/pip/pyproject-build-pip-$x.toml pyproject.toml
     "/opt/python/$x/bin/python" -c 'import sys ; sys.exit(not ((3, 6, 0) <= sys.version_info < (3, 11, 0)))' || continue
+    cp maintenance/pip/pyproject-build-pip-$x.toml pyproject.toml
     "/opt/python/$x/bin/pip" install --upgrade pip
     "/opt/python/$x/bin/pip" install --only-binary=:all: --no-cache-dir -r maintenance/pip/requirements-build-pip-$x.txt
     "/opt/python/$x/bin/pip" wheel -v . -w wheels_temp --only-binary=:all:
